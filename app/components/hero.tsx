feb23ae856e9b0d336e572c7e637d1e0a893be33
Alfonso Covarrubias <alfoncovape@gmail.com>
2024-02-01 09:01:04 -0600

    Hero height fix

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white h-screen">
-      <div className="relative isolate px-6 lg:px-8">
+    <div className="bg-white h-screen flex  justify-center items-center">
+      <div className="px-6 py-auto lg:px-8">
         <div className="mx-auto max-w-4xl py-10 sm:py-6">
           <div className="sm:mb-6 flex justify-center">
                 <img
